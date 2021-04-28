7fabcf59c1732c9cdc5b16c06f90f0da834c24af
Luke McDonald <thelukemcdonald@gmail.com>
2021-04-28 11:39:43 -0500

    Fix desktop view.

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Main({ children }) {
-	return <main className="lg:absolute lg:inset-0 lg:flex">{children}</main>;
+	return (
+		<main className="lg:flex" style={{ maxHeight: '675px' }}>
+			{children}
+		</main>
+	);
 }
