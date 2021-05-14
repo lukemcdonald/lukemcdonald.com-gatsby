b9a1f2582b6d901c10c0c94920bc7d6e7774b437
Luke McDonald <thelukemcdonald@gmail.com>
2021-05-14 12:24:14 -0500

    Remove placeholder image from featured image fragment.
    Allows image to fade in useing the media content areas background color.

--- src/utils/graphql-fragments.js
+++ src/utils/graphql-fragments.js
@@ -3,15 +3,11 @@ import { graphql } from 'gatsby';
 export const FeaturedImage = graphql`
 	fragment FeaturedImage on File {
 		childImageSharp {
-			gatsbyImageData(layout: FULL_WIDTH, transformOptions: { grayscale: true })
-		}
-	}
-`;
-
-export const SquareImage = graphql`
-	fragment SquareImage on File {
-		childImageSharp {
-			gatsbyImageData(width: 200, height: 200, layout: CONSTRAINED)
+			gatsbyImageData(
+				layout: FULL_WIDTH
+				placeholder: NONE
+				transformOptions: { grayscale: true }
+			)
 		}
 	}
 `;
