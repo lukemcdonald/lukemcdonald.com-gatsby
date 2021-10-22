56b63e7365e82dcbfd11d3eedaee880ee4d00f97
Luke McDonald <thelukemcdonald@gmail.com>
2021-10-22 11:06:53 -0500

    Update prettier settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,39 @@
 module.exports = {
   extends: ['prettier', 'kentcdodds', 'kentcdodds/react'],
+  plugins: ['prettier'],
   rules: {
     'no-console': 'off',
 
-    // meh...
+    // Meh...
     '@babel/new-cap': 'off',
     'react/jsx-filename-extension': 'off',
 
-    // this one isn't smart enough for our "~/" imports
+    // These are not smart enough for our resolved imports.
     'import/order': 'off',
     'import/no-unresolved': 'off',
 
+    // Options: https://prettier.io/docs/en/options.html
     'prettier/prettier': [
       'error',
       {
         arrowParens: 'avoid',
-        bracketSpacing: true,
-        embeddedLanguageFormatting: 'auto',
-        endOfLine: 'lf',
-        htmlWhitespaceSensitivity: 'css',
-        insertPragma: false,
-        jsxBracketSameLine: false,
-        jsxSingleQuote: false,
         printWidth: 120,
-        proseWrap: 'always',
-        quoteProps: 'as-needed',
-        requirePragma: false,
         semi: false,
         singleQuote: true,
-        tabWidth: 2,
         trailingComma: 'all',
-        useTabs: false,
+        // bracketSpacing: true,
+        // bracketSameLine: false,
+        // embeddedLanguageFormatting: 'auto',
+        // endOfLine: 'lf',
+        // htmlWhitespaceSensitivity: 'css',
+        // insertPragma: false,
+        // jsxSingleQuote: false,
+        // proseWrap: 'preserve',
+        // quoteProps: 'as-needed',
+        // requirePragma: false,
+        // tabWidth: 2,
+        // useTabs: false,
       },
     ],
   },
-  plugins: ['prettier'],
 }
